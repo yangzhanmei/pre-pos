a84444725613fca568b524efeccd7f668e06035f
yangzhanmei <1724617718@qq.com>
2017-02-19 14:04:50 +0800

    complete the section-1/prectice-1.js

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,3 +1,11 @@
 function collect_same_elements(collection_a, collection_b) {
-	
+	return collection_a.filter((a)=>{
+        if(isExit(a,collection_b)){
+          return a;
+        }
+  })
+}
+
+function isExit(a,collection_b) {
+  return collection_b.find((b)=>{return a===b})
 }
