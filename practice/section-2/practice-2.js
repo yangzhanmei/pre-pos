db8c1a331dc50762b8c775b31cacd2a34dae37a9
yangzhanmei <1724617718@qq.com>
2017-02-19 15:16:14 +0800

    complete the section-2/prectice-2.js

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,3 +1,29 @@
 function count_same_elements(collection) {
+  let result = [];
 
+  for(let c of collection) {
+    if(c.length > 1){
+      const sp = c.split("-");
+      result.push({key:sp[0],count:parseInt(sp[1])});
+    }
+    else {
+      if(!isExit(c,result)){
+        result.push({key:c,count:1});
+      }
+    }
+  }
+
+  return result;
+}
+
+function isExit(c,result) {
+  return result.find((r)=>{
+    if(c===r.key){
+      r.count ++;
+      return 1;
+    }
+    else {
+      return 0;
+    }
+  })
 }
