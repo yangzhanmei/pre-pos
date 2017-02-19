78eda784e029ecedddd46bb74b1bb560b4e35924
yangzhanmei <1724617718@qq.com>
2017-02-19 17:32:47 +0800

    complete the section-3/prectice-1.js

--- practice/section-3/practice-1.js
+++ practice/section-3/practice-1.js
@@ -1,3 +1,14 @@
 function create_updated_collection(collection_a, object_b) {
 
+  return result = collection_a.map((a)=>{
+    if(isExit(a.key,object_b.value)){
+      a.count --;
+    }
+    return a;
+  });
+}
+
+function isExit(a,object_b) {
+
+  return object_b.find((b)=>{return a===b})
 }
