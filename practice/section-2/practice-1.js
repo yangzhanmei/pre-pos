2d156cb6bbe20641295570a2bd126fc8325cead1
yangzhanmei <1724617718@qq.com>
2017-02-19 14:47:00 +0800

    complete the section-2/prectice-1.js

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,3 +1,23 @@
 function count_same_elements(collection) {
+  let result = [];
 
+  for(let c of collection) {
+    if(!isExit(c,result)){
+      result.push({key:c,count:1});
+    }
+  }
+
+  return result;
+}
+
+function isExit(c,result) {
+  return result.find((r)=>{
+    if(c===r.key){
+      r.count ++;
+      return 1;
+    }
+    else {
+      return 0;
+    }
+  })
 }
