3c3265f1cc6f36f0be74b419037bbcb801dd0fbb
yangzhanmei <1724617718@qq.com>
2017-02-19 17:47:38 +0800

    complete the section-3/prectice-3.js

--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,3 +1,36 @@
 function create_updated_collection(collection_a, object_b) {
+  let array = [];
+  let result = [];
 
+  for(let c of collection_a) {
+    if(!isExit(c,array)){
+      array.push({key:c,count:1});
+    }
+  }
+
+  return result = array.map((a)=>{
+    if(isExitInObject_b(a.key,object_b.value)){
+      a.count -= parseInt(a.count/3);
+    }
+    return a;
+  });
 }
+
+function isExitInObject_b(a,object_b) {
+
+  return object_b.find((b)=>{return a===b})
+}
+
+
+function isExit(c,array) {
+  return array.find((r)=>{
+    if(c===r.key){
+      r.count ++;
+      return 1;
+    }
+    else {
+      return 0;
+    }
+  })
+}
+
