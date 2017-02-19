73237a7172edb1a305bf6ae197b2a811712a301e
yangzhanmei <1724617718@qq.com>
2017-02-19 17:26:30 +0800

    complete the section-2/prectice-3.js

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,3 +1,33 @@
 function count_same_elements(collection) {
+  let array = [];
+  let result = [];
 
+  for(let c of collection){
+    array = c.split("");
+    if(array.length === 1){
+        result = isExit(c,result,1);
+    }
+    else if(array.length <= 4){
+        result = isExit(array[0],result,parseInt(array[2]));
+    }
+    else{
+      const count = parseInt(array[2]*10)+parseInt(array[3]);
+      result= isExit(array[0],result,count);
+    }
+  }
+
+  return result;
+}
+
+function isExit(c,result,count){
+  for(let r of result) {
+    if (c === r.name) {
+      r.summary += count;
+
+      return result;
+    }
+  }
+
+  result.push({name:c,summary:count});
+  return result;
 }
