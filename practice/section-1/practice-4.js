476d728192ab06bd019dc3750d84ee592e0b09c8
yangzhanmei <1724617718@qq.com>
2017-02-19 14:17:36 +0800

    complete the section-1/prectice-4.js

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,3 +1,15 @@
 function collect_same_elements(collection_a, object_b) {
+  const result = collection_a.filter((a)=>{
+    if(isExit(a.key,object_b.value)){
+      return a.key;
+    }
+  });
 
+  return result.map((a)=>{
+    return a.key;
+  })
+}
+
+function isExit(a,object_b) {
+  return object_b.find((b)=>{return a===b})
 }
